7e0739818a8806019385d2071059168df40615ac
Dariusz Kalinowski <dariusz.kalinowski@1und1.de>
2017-02-15 15:07:07 +0100

    sample with firebase remote config

--- app/src/main/java/sample/de/firebasesample/MainActivity.java
+++ app/src/main/java/sample/de/firebasesample/MainActivity.java
@@ -1,22 +1,28 @@
 package sample.de.firebasesample;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -89,11 +95,17 @@ public class MainActivity extends AppCompatActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+
+        public static final String DEFAULT_TEXT_KEY = "default_text";
+        FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
+        TextView mainText;
+
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+
 
         public PlaceholderFragment() {
         }
@@ -111,12 +123,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            remoteConfig.setConfigSettings(new FirebaseRemoteConfigSettings.Builder()
+                    .setDeveloperModeEnabled(true).build());
+
+            HashMap<String, Object> defaultSettings = new HashMap<>();
+            defaultSettings.put(DEFAULT_TEXT_KEY, "This is sample text");
+
+            remoteConfig.setDefaults(defaultSettings);
+            Task<Void> fetch = remoteConfig.fetch(0);
+            fetch.addOnSuccessListener(getActivity(), new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    remoteConfig.activateFetched();
+                    updateMainText();
+                }
+            });
+        }
+
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            mainText = (TextView) rootView.findViewById(R.id.content_text);
+
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            updateMainText();
             return rootView;
+        }
+
+        private void updateMainText() {
+            mainText.setText(remoteConfig.getString(DEFAULT_TEXT_KEY));
         }
     }
 
